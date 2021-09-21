a2401ab0dcac3a866565062033f7a9650afa9af4
Ibnuwulidafisyawal <ibnuwulfisyawal@gmail.com>
2021-09-21 23:05:44 +0700

    setter and getter

--- OOP.php
+++ OOP.php
@@ -1,14 +1,16 @@
 <?php 
 
+//belajar OOP PHP
 echo "<h2>Project Produk </h2>";
 echo "<hr>";
-class produk {
-    public $judul,
-           $penulis,
-           $penerbit;
 
-    protected $diskon = 0,
-              $harga;
+class produk {
+    private $judul,
+            $penulis,
+            $penerbit,
+            $harga,
+            $diskon = 0;
+           
 
         public function __construct( $judul = "judul", $penulis= "penulis", $penerbit= "penerbit", $harga=0) { 
 
@@ -19,23 +21,64 @@ class produk {
             
             }
 
-            //set diskon untuk produk 
-            public function setDiskon( $diskon){
-                 $this->diskon = $diskon;
+            //dikarenakan property  nya private maka dibuat method getjudul dll agar bisa di akses di objek
+            //ini dinamakan dengan getter oop
+
+            //dan setjudul atau dll  untuk mengganti nama dari produk komik atau game dan bisa di set di objek 
+
+            public function setJudul($judul) { 
+                // set judul harus string
+                if (!is_string($judul)) {
+                    throw new Exception("Judul ini Harus String");
+                }
+                $this->judul=$judul;
             }
-        
-            //get harga produk
+            public function getJudul() {
+                return $this->judul;
+            }
+
+            public function setPenulis($penulis){
+                $this->penulis= $penulis;
+            }
+            public function getPenulis(){
+                return $this->penulis;
+            }
+
+            public function setPenerbit($penerbit){
+                $this->penerbit= $penerbit;
+            }
+            public function getPenerbit(){
+                return $this->penerbit;
+            }
+
+            public function setHarga($harga){
+                $this->harga= $harga;
+            }
+            //get Harga untuk diskon 
             public function getHarga(){
                 return $this->harga - ($this->harga * $this->diskon / 100);
             }
+            //set diskon untuk produk 
+            public function setDiskon( $diskon) {
+                 $this->diskon = $diskon;
+            }
 
-         public function getLabel(){
-            return "$this->penulis, $this->penerbit";
+            public function getDiskon(){
+                return $this->diskon;
+            }
+    
             
-         }
-         //get info produk untuk menampilkan dari class komik dan game
-         public  function getInfoProduk()
-         {
+        
+         
+          
+
+            public function getLabel(){
+                return "$this->penulis, $this->penerbit";
+                
+            }
+            //get info produk untuk menampilkan dari class komik dan game
+            public  function getInfoProduk()
+            {
             $str = "{$this->judul} | {$this->getlabel()} (RP. {$this->harga})";
                 // if ($this->tipe == "Komik") {
                 //    $str .= " - {$this->jmlhlmn} Halaman.";
@@ -43,7 +86,7 @@ class produk {
                 //     $str .= " - {$this->jam} Jam.";
                 // }
             return $str;
-         }
+            }
 }
 
 //extends adalah untuk memanggil class induk 
@@ -100,12 +143,20 @@ echo "<br>";
 echo "<hr>";
 
 //tampilkan diskon 
-echo "Harga " ,"Diskon ", $produk1->judul , $produk1->setDiskon(50);
+echo "Harga " ,"Diskon ", $produk1->getJudul() , $produk1->setDiskon(30);
 echo $produk1->getHarga();
 echo  "<br>";
-echo "Harga " ,"Diskon ", $produk2->judul , $produk2->setDiskon(50);
+echo "Harga " ,"Diskon ", $produk2->getJudul() , $produk2->setDiskon(50);
 echo $produk2->getHarga();
-echo "<br>";
+echo "<hr>";
+
+echo "ini setter untuk mengganti nama judul dari produk tanpa mengubah nama property dari produk ";
+echo $produk1->setJudul("<b>Saya Maung</b>");
+
+//getter untuk menampilkan property judul dari property private
+echo $produk1->getJudul();
+
+
 // $infoProduk1 = new cetakInfoProduk();
 // echo $infoProduk1->cetak($produk1);
 ?>
