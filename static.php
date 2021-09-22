9b3bcfc9762b8d2832fc7c63261fb6733f66e768
Ibnuwulidafisyawal <ibnuwulfisyawal@gmail.com>
2021-09-22 16:42:14 +0700

    static

--- static.php
+++ static.php
@@ -16,6 +16,8 @@ class contohStatic{
 
 }
 
+
+
 $obj1= new contohStatic();
 echo $obj1->welcome();
 echo $obj1->welcome();
@@ -27,7 +29,30 @@ $obj2 = new contohStatic();
 echo $obj2->welcome();
 echo $obj2->welcome();
 echo $obj2->welcome();
+echo "<hr>";
+
+
+
+//contoh static ke 2
+class Mobil {
+    public static $speed  = 200;
+
+    public static function getDefaultspeed() {
+        return self::$speed;
+    }
+
+    public static function getspeed(){
+        return 400;
+    }
+}
+
+echo Mobil::getspeed();
+echo "<br>";
+echo Mobil::$speed; 
+echo "<br>";
+echo Mobil::getDefaultspeed(); 
 
 
 //jadi static memudahkan dan lebih hemat code
+
 ?>
